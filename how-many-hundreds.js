2fca3d9dbabc2a17c38df1abb73549982302dd53
Mark Bieganek <mark.bieganek@telus.com>
2022-11-02 15:42:38 -0600

    Add challenge with initial setup & comments

---
+++ how-many-hundreds.js
@@ -0,0 +1,33 @@
+/* 
+As humanity gets closer to living on Mars, we must start sending 
+supplies to the red planet now, to prepare for our arrival. So Canada 
+is going to start to send containers of maple syrup. Each container on 
+the space ship can hold exactly 100 bottles of maple syrup. A 
+container will only be sent if it's full.
+
+Given a certain number of bottles, we need to determine how many 
+containers can be filled and sent. In this challenge, we will write a 
+function that determines how many hundreds can be made from a number.
+
+EXPECTED OUTPUT:
+When this function is given a number, it should return how many 
+hundreds fit into that number.
+
+howManyHundreds(1000);  // 10
+howManyHundreds(894);	  // 8
+howManyHundreds(520);	  // 5
+howManyHundreds(99);	  // 0
+howManyHundreds(0);	    // 0
+
+ALGORITHM:
+- 
+*/
+
+/**
+ * A function that determines how many hundreds can be made from a number.
+ * @param {number} number The input number
+ * @returns {number} The number of hundreds that can be made
+ */
+ const howManyHundreds = (number) => {
+   
+ }

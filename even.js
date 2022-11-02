a211d86f7fa03f8c45b7425eed3d033589e13c1f
Mark Bieganek <mark.bieganek@telus.com>
2022-11-02 10:22:03 -0600

    Refactor test cases for simlicity

--- even.js
+++ even.js
@@ -7,9 +7,6 @@ const isEven = function (num) {
   return num % 2 === 0;
 }
 
-// Test cases.
-const tenIsEven = isEven(10);
-const elevenIsEven = isEven(11);
 // Print test cases.
-console.log(tenIsEven);
-console.log(elevenIsEven);
+console.log(isEven(10));
+console.log(isEven(11));

1a5d928531a4d451085ea8bb29d8bcd40b6499e7
Mark Bieganek <mark.bieganek@telus.com>
2022-11-02 10:20:33 -0600

    Add test cases and print them to console

--- even.js
+++ even.js
@@ -6,3 +6,10 @@
 const isEven = function (num) {
   return num % 2 === 0;
 }
+
+// Test cases.
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+// Print test cases.
+console.log(tenIsEven);
+console.log(elevenIsEven);

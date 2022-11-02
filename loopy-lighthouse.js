37e17241f24f703db94ddcb021d093e657155123
Mark Bieganek <mark.bieganek@telus.com>
2022-11-01 19:40:47 -0600

    Change output for Lighthouse numbers

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -21,6 +21,9 @@ for (i = 100; i <= 200; i++) {
   // Loopy numbers are printed as "Loopy" instead.
   if (i % 3 === 0) {
     console.log("Loopy");
+  // Lighthouse numbers are printed as "Lighthouse" instead.
+  } else if (i % 4 === 0) {
+    console.log("Lighthouse");
   } else {
     // Print the number.
     console.log(i);

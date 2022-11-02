6ee5ae16345ccad87f9305028db52b6db9ddf785
Mark Bieganek <mark.bieganek@telus.com>
2022-11-01 19:27:27 -0600

    Add comments describing the challenge

---
+++ loopy-lighthouse.js
@@ -0,0 +1,17 @@
+/*
+
+Challenge
+**************
+We will be writing a program that prints the numbers from 100 to 200 to
+the console, with three exceptions:
+
+  - If the number is a multiple of 3, print the string "Loopy" instead
+    of the number.
+  - If the number is a multiple of 4, print the string "Lighthouse"
+    instead of the number.
+  - If the number is a multiple of both 3 and 4, print the string 
+    "LoopyLighthouse" instead of the number.
+
+By print, we are referring to console.log.
+
+*/

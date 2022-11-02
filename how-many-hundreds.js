c9bd36f1c3e620ab3cec1e8168bbe251e590e8c6
Mark Bieganek <mark.bieganek@telus.com>
2022-11-02 15:55:38 -0600

    Test cases now print to console

--- how-many-hundreds.js
+++ how-many-hundreds.js
@@ -51,8 +51,8 @@ ALGORITHM:
  }
 
 // Test cases
-howManyHundreds(1000);  // 10
-howManyHundreds(894);	  // 8
-howManyHundreds(520);	  // 5
-howManyHundreds(99);	  // 0
-howManyHundreds(0);	    // 0
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);

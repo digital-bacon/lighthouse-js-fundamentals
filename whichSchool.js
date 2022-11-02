fa8aa097bf2b538e0305156a3be7b6726b1e177c
Mark Bieganek <mark.bieganek@telus.com>
2022-11-01 19:21:59 -0600

    Refactor to use if...then conditional

--- whichSchool.js
+++ whichSchool.js
@@ -22,16 +22,34 @@ The function returns one of the following strings, based on the value of age:
  */
 const whichSchool  = function (age) {
   switch (true) {
+    // Students over 18 attend Lighthouse Labs.
     case (age >= 19):
-      return "Lighthouse Labs"; // Students over 18 attend Lighthouse Labs.
+      return "Lighthouse Labs";
+    // Students between 13 and 18 attend Secondary School.  
     case (age >= 13 && age <= 18):
-      return "Secondary School"; // Students between 13 and 18 attend Secondary School.
-    case (age >= 1 && age < 18):
-      return "Elementary School"; // Students between 1 and 18 attend Elementary School.
+      return "Secondary School"; 
+    // Students between 1 and 13 attend Elementary School.
+    case (age >= 1 && age < 13):
+      return "Elementary School";
+    // Students must be at least 1 year old to attend a school.
     default:
-      return "Students must be at least 1 year old to attend school."
+      return "You aren't old enough to attend school yet."
   }
 }
+
+/*
+const whichSchool  = function (age) {
+  if (age === 0) { // Students must be at least 1 year old to attend a school.
+    return "You aren't old enough to attend school yet.";
+  } else if (age >= 1 && age < 13) { // Students between 1 and 13 attend Elementary School.
+    return "Elementary School";
+  } else if (age >= 13 && age <= 18) { // Students between 13 and 18 attend Secondary School.
+    return "Secondary School"; 
+  } else if (age >= 19) { // Students over 18 attend Lighthouse Labs. 
+    return "Lighthouse Labs";
+  }
+}
+*/
 
 console.log("I am 35. Which school should I go to?");
 console.log(whichSchool(35));

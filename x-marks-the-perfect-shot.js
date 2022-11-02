ef578d6048ece9f0c63ee331443d1d3fe5bbcd15
Mark Bieganek <mark.bieganek@telus.com>
2022-11-02 13:33:45 -0600

    Add function to calculate movement

--- x-marks-the-perfect-shot.js
+++ x-marks-the-perfect-shot.js
@@ -25,13 +25,28 @@ The finalPosition function should return the array:
 
 ALGORITHM:
 - `finalPosition(moves)` is called with the moves array as an argument.
+- Add a way to perform movement calculation.
+  - Create a function named calcMovement with parameters `direction` 
+  and `coordinates`.
+    - the function will read the direction provided, and then update
+    the provided coordinates accordingly.
+        - North = +1 on the x axis.
+          - calculated as `coordinates[1] += 1`
+        - South = -1 on the x axis.
+          - calculated as `coordinates[1] -= 1`
+        - East = +1 on the y axis.
+          - calculated as `coordinates[0] += 1`
+        - West = -1 on the y axis. 
+    - the function will return the result of the calculation as an 
+    array representing [x,y] coordinates.
 - within `finalPosition()`:
   - Add a way to store the final x,y coordinates which will be output 
   by this function  
     - Create an array variable named coordinates with initial value of 
     [0,0] to match the default starting position.
     - Iterate through the provided moves argument array.
-      - print the current move to the console.
+      - Call `calcMovement(move, coordinates)` on each array item, and return the result
+      to `coordinates`.
   - Return 'coordinates'.
 */
 
@@ -40,17 +55,34 @@ ALGORITHM:
  * representing the position of the parade after completing all of the 
  * provided moves.
  * @param {Array.<string>} moves Parade movements, values can be 'north', 'east', 'south', 'west'
- * @returns {Array.<number>} With the [x,y] coordinates after all movements have been calculated
+ * @returns {Array.<number,number>} With the [x,y] coordinates after all movements have been calculated
  */
  const finalPosition = function (moves) {
-  // Add an array to record our current position. The parade starts at [0,0];
-  let coordinates = [0,0];
+  // Current position as x,y coordinates.
+  let coordinates = [0, 0];
   // Calculate moves.
   for (const move of moves) {
-    console.log(move);
+    coordinates = calcMovement(move, coordinates);
   }
   // Print the current location to console.
-  console.log(coordinates);
+  return coordinates;
+}
+
+/**
+ * 
+ * @param {string} direction Values can be 'north', 'east', 'south', 'west'
+ * @param {Array.<number,number>} coordinates With initaly [x,y] coordinates
+ * @returns {Array.<number,number>} With the [x,y] coordinates after all movements have been calculated
+ */
+function calcMovement (direction, coordinates) {
+  switch (true) {
+    case (direction === 'north') : coordinates[1] += 1; break; // Move 1 grid square north.
+    case (direction === 'south') : coordinates[1] -= 1; break; // Move 1 grid square south.
+    case (direction === 'east') : coordinates[0] += 1; break; // Move 1 grid square east.
+    case (direction === 'west') : coordinates[0] -= 1; break; // Move 1 grid square west.
+    default: break;
+  }
+  return coordinates;
 }
 
 // Dataset of parade movements.

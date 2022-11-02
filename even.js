9ac428f6cfd74bfb938ba3ce4cbe05fb8c3dccff
Mark Bieganek <mark.bieganek@telus.com>
2022-11-02 10:18:27 -0600

    Add function to test if a number is even

---
+++ even.js
@@ -0,0 +1,8 @@
+/**
+ * Function to test if a number is even.
+ * @param {number} num - Any number.
+ * @returns {boolean} - Indicates if the number is even.
+ */
+const isEven = function (num) {
+  return num % 2 === 0;
+}

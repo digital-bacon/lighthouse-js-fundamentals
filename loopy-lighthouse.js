0d0319b6e7b4a5e9db8deb16ade74227732899a1
Mark Bieganek <mark.bieganek@telus.com>
2022-11-01 20:00:53 -0600

    Refactor for readability with switch statement

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -17,18 +17,21 @@ By print, we are referring to console.log.
 */
 
 // Print numbers from 100 to 200, inclusive.
-for (i = 100; i <= 200; i++) {
-  // LoopyLighthouse numbers are printed as "LoopyLighthouse" instead.
-  if ((i % 3 === 0) && (i % 4 === 0)) {
+// If the number is a multiple of 3, print the string "Loopy" instead of the number.
+// If the number is a multiple of 4, print the string "Lighthouse" instead of the number.
+// If the number is a multiple of both 3 and 4, print the string "LoopyLighthouse" instead of the number.
+for (let i = 100; i <= 200; i++) {
+  switch (true) {
+  case ((i % 3 === 0) && (i % 4 === 0)): // A number that's a multiple of 3 and 4.
     console.log("LoopyLighthouse");
-  // Loopy numbers are printed as "Loopy" instead.
-  } else if (i % 3 === 0) {
+    break;
+  case (i % 3 === 0): // A number that's a multiple of 3.
     console.log("Loopy");
-  // Lighthouse numbers are printed as "Lighthouse" instead.
-  } else if (i % 4 === 0) {
+    break;
+  case (i % 4 === 0): // A number that's a multiple of 4.
     console.log("Lighthouse");
-  } else {
-    // Print the number.
+    break;
+  default: // A number that's not a multiple of 3 or 4.
     console.log(i);
   }
 }

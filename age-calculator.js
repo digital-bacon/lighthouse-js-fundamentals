c4af7855a9f2daeec3b9cfc5e83b577628094056
Mark Bieganek <mark.bieganek@telus.com>
2022-11-02 15:19:00 -0600

    Add challenge with initial setup & comments

---
+++ age-calculator.js
@@ -0,0 +1,35 @@
+/* 
+We've been given access to a database that contains all of the users 
+for the PlentyOfDevs dating site. This database contains every user's 
+date of birth, but users have complained about having to figure out 
+someone's age using just their date of birth. In this challenge, we 
+will write a function that determines someone's age, given their date 
+of birth.
+
+Define a function called ageCalculator. This function should take 3 parameters:
+
+  - @name – a string representing someone's name
+  - @yearOfBirth – a number representing their year of birth
+  - @currentYear – a number representing the current year
+
+EXPECTED OUTPUT:
+The ageCalculator function should return a string explaining how old 
+the person is. For example, if we called: 
+`ageCalculator("Suzie", 1983, 2015);`, the return value should be as 
+follows. "Suzie is 32 years old."
+
+ALGORITHM:
+- 
+
+*/
+
+/**
+ * A function that returns a string explaining how old a person is.
+ * @param {string} name A person's name
+ * @param {number} yearOfBirth The year the person was born
+ * @param {number} currentYear The calendar year
+ * @returns {string>} A string explaining how old a person is
+ */
+ const ageCalculator = function (name, yearOfBirth, currentYear) {
+
+ }

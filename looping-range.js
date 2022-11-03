751bf0cabc489be6a43bff783c18be0456bfce34
Mark Bieganek <mark.bieganek@telus.com>
2022-11-02 20:10:43 -0600

    Add challenge with initial setup & comments

---
+++ looping-range.js
@@ -0,0 +1,49 @@
+/* 
+Creating a long array of repeating numbers can be a time consuming 
+task. Imagine creating an array of every even number from 0 to 100. In 
+this challenge, we will build a function that creates an array like 
+this for a given start, end, and step parameter.
+
+Define a function called range
+
+EXPECTED OUTPUT:
+When the function is given 3 integer arguments, the output should be
+as follows.
+
+range(0, 10, 2);	// [ 0, 2, 4, 6, 8, 10 ]
+range(10, 30, 5);	// [ 10, 15, 20, 25, 30 ]
+range(-5, 2, 3);	// [ -5, -2, 1 ]
+
+CONSTRAINTS:
+1. The function takes 3 integer parameters: start, end, and step.
+2. The function should return an array of numbers from start to end 
+counting by step.
+3. The function should return an empty array [] if given incorrect 
+parameters, such as:
+  - @start, @end, or @step being undefined
+  - @start being greater than @end
+  - @step being 0, or negative
+
+BREAK IT DOWN:
+- 
+
+ALGORITHM:
+- 
+
+*/
+
+/**
+ * 
+ * @param {number} start A positive or negative integer
+ * @param {*} end A positive or negative integer
+ * @param {*} step A positive or negative integer 
+ * @returns {Array<numbers>} An array of numbers from start to end counting by step
+ */
+ const range = (start, end, step) => {
+
+ }
+
+// Test cases
+console.log(range(0, 10, 2), "=?", [ 0, 2, 4, 6, 8, 10 ]);
+console.log(range(10, 30, 5), "=?", [ 10, 15, 20, 25, 30 ]);
+console.log(range(-5, 2, 3), "=?", [ -5, -2, 1 ]);

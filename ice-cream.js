35c1f6cb92f458e966a8d3d51cbc76c87f6e18da
Mark Bieganek <mark.bieganek@telus.com>
2022-11-02 19:46:15 -0600

    Add a flavour, then print them

--- ice-cream.js
+++ ice-cream.js
@@ -1,3 +1,17 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+iceCreamFlavours.push('root beer'); // Add a flavour
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]); // The first flavour
+
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]); // The last flavour
+
+console.log(iceCreamFlavours.length); // The total flavours
 /* 
 The freezer at Lighthouse Labs is overflowing with Ice Cream so we 
 need to have an ice cream party for all the students so it can be 

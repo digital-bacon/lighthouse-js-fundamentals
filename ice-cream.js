aace712b982298ec6b868977e0096380def2ca92
Mark Bieganek <mark.bieganek@telus.com>
2022-11-02 19:02:15 -0600

    Add assignment with some comments

---
+++ ice-cream.js
@@ -0,0 +1,6 @@
+/* 
+The freezer at Lighthouse Labs is overflowing with Ice Cream so we 
+need to have an ice cream party for all the students so it can be 
+eaten. But before we can do that, we need determine which flavours of 
+ice cream we have, and we'll need to organize them for easy access.
+*/

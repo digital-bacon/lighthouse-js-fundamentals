08d5f711394714ed8c67925027556f719bb17a0c
Mark Bieganek <mark.bieganek@telus.com>
2022-11-02 19:47:46 -0600

    Use a while loop to print the items

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,15 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+// Use a for loop to print the items
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+// Use a while loop to print the items
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }

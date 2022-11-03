be206106ac1f697df529a32157e5562f679848d5
Mark Bieganek <mark.bieganek@telus.com>
2022-11-03 16:41:50 -0600

    Simplify the function and conform to challenge objective

--- objects-trash-to-treasure.js
+++ objects-trash-to-treasure.js
@@ -52,50 +52,43 @@ minimum the following keys:
 
 ALGORITHM smartGarbage():
 - We need a way to ensure the trash type is valid.
-  - Create an array of valid trash types.
-  - Use `array.indexOf(trash)` to see if the provided trash type is
-  found in the array.
-    - if not found, -1 will be returned.
-    - if found, the index position matching 0 or more will be returned
-- We need a way to verify if a key exists programmatically
-  - use the `in` keyword to check for the key's existence in `bin`
+- We need a way to ensure the `bins` object contains the required keys 
+  - use `trash in bins` to check if a key exists in the bins object
+  - if it doesn't exist, return `bins` unchanged
 - We need a way to modify a key directly, without knowing the name of 
 the key.
   - because the trash parameter contains the value of the key to 
-  modify, we can use ES6 
+  modify, we can use ES6 template literal notation to reference the 
+  key dynamically and increase it's value
+- return the updated bin
 */
 
 /**
- * Function that increases the garbage count inside a bin for waste, 
+ * Function that increases the garbage count inside a bins for waste, 
  * recycling, or compost depending on what trash is submitted.
  * @param {string} trash The type of trash being added from waste, recycling, or compost
- * @param {Object} bin The trash bin that the waste is being added to
- * @returns {Object} The updated trash bin object
+ * @param {Object} bins The trash bins that the trash is being added to
+ * @returns {Object} The updated trash bins object
  */
- const smartGarbage = (trash, bin) => {
-  // Ensure the trash being added is accepted in the bin
-  if (['compost', 'waste', 'recycling'].indexOf(trash) === -1) {
-    return bin; // return with no changes
-  }
-  // Ensure the trash being added already exists in the bin
-  if (trash in bin) {
-    // Add the trash to the existing trash in the bin
-    bin[`${trash}`]++;
+ const smartGarbage = function (trash, bins) {
+  // Ensure the provided bins accept this kind of trash
+  if (trash in bins === false) {
+    return bins; // the bins don't accept this kind of trash
   } else {
-    // Add a new pile of trash in the bin
-    bin[`${trash}`] = 1;
-  };
-  return bin;
+    // Add this trash to the appropriate bin
+    bins[`${trash}`]++;
+  }
+  return bins;
  }
 
 // Test cases, using valid arguments
-// Add recycling to bin with at least 1 item existing of each trash type
+// Add recycling to bins with at least 1 item existing of each trash type
 console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 }), "=?", { waste: 4, recycling: 3, compost: 5 });
-// Add waste to bin with at least 1 item existing of each trash type
+// Add waste to bins with at least 1 item existing of each trash type
 console.log(smartGarbage('waste', { waste: 4, recycling: 2, compost: 5 }), "=?", { waste: 5, recycling: 2, compost: 5 });
-// Add compost to bin with at least 1 item existing of each trash type
+// Add compost to bins with at least 1 item existing of each trash type
 console.log(smartGarbage('compost', { waste: 4, recycling: 2, compost: 5 }), "=?", { waste: 4, recycling: 2, compost: 6 });
-// Test add recycling to bin when no recycling exists in the bin
-console.log(smartGarbage('recycling', { waste: 4, compost: 5 }), "=?", { waste: 4, compost: 5, recycling: 1 });
-// Test add recycling to bin when the bin is empty
-console.log(smartGarbage('recycling', { }), "=?", { recycling: 1 });
+// Test add recycling to bins when no recycling exists in the bin
+console.log(smartGarbage('recycling', { waste: 4, compost: 5 }), "=?", { waste: 4, compost: 5});
+// Test add recycling to bins when the bins is empty
+console.log(smartGarbage('recycling', { }), "=?", { });

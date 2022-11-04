49845ab9c2aab63f25928e3848ff086cc62f6232
Mark Bieganek <mark.bieganek@telus.com>
2022-11-04 17:51:32 -0600

    Return the largest number found in an array

--- kata-01-sum-the-largest-numbers.js
+++ kata-01-sum-the-largest-numbers.js
@@ -36,32 +36,41 @@ CONSTRAINTS:
 1. Do not use built-in helper functions like `.sort`, `.map`, `.filter`
 
 ALGORITHM:
-We need a way to find the highest value in a data collection
-- Create a way to remember the highest number found so far
-- Create a way to remember the position of the highest number found 
+We need a way to find the largest value in a data collection
+- Create a way to remember the largest number found so far
+- Create a way to remember the position of the largest number found 
 so far from the data collection
 STEP 1. Read through the data collection
-  - If the value of the current record is larger than the highest 
-  number found so far, update the highest number found so far to 
+  - If the value of the current record is larger than the largest 
+  number found so far, update the largest number found so far to 
   reflect this number, and also record where to find it in the data 
   collection
 STEP 2.When done reading through the records
-  - Go back to the highest number we found in the records
+  - Go back to the largest number we found in the records
   - Remove the number from the data collection
   - Remember the number somewhere else so we can come back to it 
   later
-- Repeat the two steps above to find the second highest number.
-- Add the two highest numbers together and return the result.
+- Repeat the two steps above to find the second largest number.
+- Add the two largest numbers together and return the result.
 
 */
 
 /**
  * Function that returns the sum of the two largest numbers in an array
  * @param {Array,<number>} data An array of numbers
- * @returns {number} The sum of the two highest numbers
+ * @returns {number} The sum of the two largest numbers
  */
  const sumLargestNumbers = function(data) {
-   
+   let largestNumbers = [];
+   let largestNumber = 0;
+   let index;
+   for (let i = 0; i < data.length; i++) {
+    if (data[i] >= largestNumber) {
+      largestNumber = data[i];
+      index = i;
+    }
+   }
+   return largestNumber;
  }
 
  console.log(sumLargestNumbers([1, 10]), "=?", 11);

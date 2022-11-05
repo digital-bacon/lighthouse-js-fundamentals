aca314474fe8a1c5513590d142f170bbd4a20d14
Mark Bieganek <mark.bieganek@telus.com>
2022-11-04 21:59:04 -0600

    Add more space between Challenge and Instruction comments

--- kata-03-vowels.js
+++ kata-03-vowels.js
@@ -5,6 +5,7 @@ In this exercise, we will be counting the number of vowels that appear
 in a given string. For this exercise, consider the following to be 
 vowels: a, e, i, o, and u.
 
+
 INSTRUCTION:
 Create a function named numberOfVowels that will receive a string and 
 return the number of vowels in that string.

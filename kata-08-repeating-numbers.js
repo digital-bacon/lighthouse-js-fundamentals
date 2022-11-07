9ad6ecd74d271290b1a29e747b5b8afa7da03221
Mark Bieganek <mark.bieganek@telus.com>
2022-11-06 22:12:49 -0700

    Repeats strings successfully, but without commas

--- kata-08-repeating-numbers.js
+++ kata-08-repeating-numbers.js
@@ -1,5 +1,15 @@
 const repeatNumbers = function(data) {
-
+  let output = '';
+  // Loop data array
+  for (let i = 0; i < data.length; i++) {
+    let repeatString = data[i][0];
+    let repeatAmount = data[i][1];
+    // Repeat the string
+    for (let y = 0; y < repeatAmount; y++) {
+        output += repeatString;
+    }
+  }
+  return output;
 };
 
 console.log(repeatNumbers([[1, 10]]));

7200124cec10cce52287a2f14084a3ef318d1f0f
Mark Bieganek <mark.bieganek@telus.com>
2022-11-07 12:24:21 -0700

    Add a way to perform exponent calculation without Math.pow()

--- kata-11-bouncy-castles.js
+++ kata-11-bouncy-castles.js
@@ -39,6 +39,24 @@ const totalVolume = function (solids) {
   // Code here? Yup!
 }
 
+/**
+ * Function that calculates the product of exponent multiplication
+ * @param {number} base 
+ * @param {number} power 
+ * @returns {number} Result of calculations
+ */
+ function mathPower(base, power) { 
+  let result;
+  if (power === 0) return 1; // Power of 0 always = 1
+  // Perform calculation
+  result = base * mathPower(base, Math.abs(power) - 1);
+  // If `power` is negative, the result is divided by 1
+  if (power < 0) {
+    result = 1 / result;
+  }
+  return result;
+}
+
 // Use the value below whenever you need the value of Pi
 const PI = 3.14159 ;
 

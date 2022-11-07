4280e91ffe4f0d7bb68ce976a53c357d75c938f4
Mark Bieganek <mark.bieganek@telus.com>
2022-11-06 23:00:02 -0700

    Add function logic, returns camelCase string

--- kata-09-case-maker.js
+++ kata-09-case-maker.js
@@ -4,7 +4,21 @@
  * @returns {string} The converted string
  */
 const camelCase = function(input) {
-
+  let newString = '';
+  let capitalizeNext = false;
+  // Find spaces, and capitalize next
+  for (let i = 0; i < input.length; i++) {
+    // Find a match to the character in @itemToFind
+    if (input[i] === ' ') {
+      capitalizeNext = true; // Flag next letter for capitalization
+    } else if (capitalizeNext) {
+      newString += input[i].toUpperCase();
+      capitalizeNext = false; // Reset flag
+    } else {
+      newString += input[i];
+    }
+  }
+  return newString;
 };
 
 console.log(camelCase("this is a string"));

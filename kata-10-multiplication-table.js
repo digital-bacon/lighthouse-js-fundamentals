f2ff68170d611d6d7731c6ddd0ad9b4de5a0e5dc
Mark Bieganek <mark.bieganek@telus.com>
2022-11-06 23:20:08 -0700

    Add exercise and initial setup

---
+++ kata-10-multiplication-table.js
@@ -0,0 +1,12 @@
+/**
+ * Function that creates a multiplication table up to a given value
+ * @param {number} maxValue The highest value of the table
+ * @returns {string} The multiplication table
+ */
+ const multiplicationTable = function(maxValue) {
+  
+};
+
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));

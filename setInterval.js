a639ca08836370529f407bea1ed60bfd50def06b
Mark Bieganek <mark.bieganek@telus.com>
2022-11-07 11:54:33 -0700

    Update for more functional use

--- setInterval.js
+++ setInterval.js
@@ -1,6 +1,13 @@
-const sayHello = function() {
-  console.log("Hello");
+const sayHello = function(string, count) {
+  // Now printing 'Hello'...
+  if (count === 1) { // Stop the press!
+    clearInterval(timer);
+    return string;
+  }
+  return string + sayHello(string, count - 1);
 }
 const timer = setInterval(sayHello, 3000);
 
-clearInterval(timer);
+console.log(sayHello("Hello\n", 5));
+
+// clearInterval(timer);

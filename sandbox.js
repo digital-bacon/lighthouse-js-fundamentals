de51c317b4b314db2a685922d9a4182b35fa24d1
Mark Bieganek <mark.bieganek@telus.com>
2022-11-06 22:23:15 -0700

    Play with recursion to repeat a string

---
+++ sandbox.js
@@ -0,0 +1,21 @@
+const repeatNumbers = function(data) {
+  let output = '';
+  for (let i = 0; i < data.length; i++) {
+    output += repeat(data[i][0].toString(), data[i][1] -1);
+    if ((i + 1) < data.length) output += ', ';
+  }
+
+  return output;
+};
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
+
+// Recursion
+function repeat(string, repeatCount) { 
+  if (repeatCount === 0) return string;
+  return string + repeat(string, repeatCount - 1);
+}
+
+console.log(repeat("abc", 3))

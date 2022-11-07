b31898dd4ba39e6f78ae1e0acf7bdd755a120ac0
Mark Bieganek <mark.bieganek@telus.com>
2022-11-06 23:42:14 -0700

    Add multiplication table logic, passing all tests

--- kata-10-multiplication-table.js
+++ kata-10-multiplication-table.js
@@ -3,10 +3,17 @@
  * @param {number} maxValue The highest value of the table
  * @returns {string} The multiplication table
  */
- const multiplicationTable = function(maxValue) {
-  
+const multiplicationTable = function(maxValue) {
+  let table = '';
+  for (let multiplier = 1; multiplier <= maxValue; multiplier++) {
+    for (let number = multiplier; number <= (multiplier * maxValue); number += multiplier) {
+      table += number + ' ';
+    }
+    table += '\n';
+  }  
+  return table;
 };
 
-console.log(multiplicationTable(1));
+//console.log(multiplicationTable(1));
 console.log(multiplicationTable(5));
-console.log(multiplicationTable(10));
+//console.log(multiplicationTable(10));

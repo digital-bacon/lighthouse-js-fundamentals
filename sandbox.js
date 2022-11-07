60eb07704d27df3d9bc2d5f9e1648b54359b4665
Mark Bieganek <mark.bieganek@telus.com>
2022-11-07 12:23:37 -0700

    Play with recursion to create my own Math.pow() function

--- sandbox.js
+++ sandbox.js
@@ -12,10 +12,27 @@ const repeatNumbers = function(data) {
 // console.log(repeatNumbers([[1, 2], [2, 3]]));
 // console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
 
-// Recursion
+// Recursion to repeat a string
 function repeat(string, repeatCount) { 
   if (repeatCount === 0) return string;
   return string + repeat(string, repeatCount - 1);
 }
 
-// console.log(repeat("abc", 3))
+// console.log(repeat("abc", 3));
+
+// Recursion to perform exponent multiplication
+function mathPower(base, power) { 
+  let result;
+  if (power === 0) return 1; // Power of 0 always = 1
+  // Perform calculation
+  result = base * mathPower(base, Math.abs(power) - 1);
+  // If `power` is negative, the result is divided by 1
+  if (power < 0) {
+    result = 1 / result;
+  }
+  return result;
+}
+
+console.log(mathPower(2, 1));
+console.log(mathPower(2, 3));
+console.log(mathPower(2, -2));

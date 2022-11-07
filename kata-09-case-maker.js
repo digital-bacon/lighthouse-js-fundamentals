cbb065716b0450a64f91dcca25f46295368216ac
Mark Bieganek <mark.bieganek@telus.com>
2022-11-06 22:59:24 -0700

    Add exercise and initial setup

---
+++ kata-09-case-maker.js
@@ -0,0 +1,12 @@
+/**
+ * Function that converts a string to camelCase
+ * @param {string} input The string to convert to camelCase
+ * @returns {string} The converted string
+ */
+const camelCase = function(input) {
+
+};
+
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));

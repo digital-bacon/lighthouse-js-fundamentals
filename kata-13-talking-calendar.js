09a8e0de90830a8b50cf76732cb33432f36605b0
Mark Bieganek <mark.bieganek@telus.com>
2022-11-08 08:43:44 -0700

    Add exercise and initial setup

---
+++ kata-13-talking-calendar.js
@@ -0,0 +1,14 @@
+/**
+ * Function that converts a provided date from YYYY/MM/DD format, to 
+ * spoken date format. Ie: a date of `2017/12/02` would be converted 
+ * to `December 2nd, 2017`
+ * @param {string} date - Formatted YYYY/MM/DD date as a string
+ * @returns {string} The converted date in spoken date format
+ */
+const talkingCalendar = function(date) {
+  // Your code here
+};
+
+// console.log(talkingCalendar("2017/12/02"), '=?', 'December 2nd, 2017');
+// console.log(talkingCalendar("2007/11/11"), '=?', 'November 11th, 2007');
+// console.log(talkingCalendar("1987/08/24"), '=?', 'August 24th, 1987');

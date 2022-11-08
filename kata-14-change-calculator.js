c668c93cc5519cc16a47f935f5e24f85155ccc05
Mark Bieganek <mark.bieganek@telus.com>
2022-11-08 15:45:02 -0700

    Add exercise and initial setup

---
+++ kata-14-change-calculator.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @param {number} total - Total transaction value in cents
+ * @param {number} cash - Total payment received in cents
+ * @returns {{denominations: number}}
+ */
+const calculateChange = function(total, cash) {
+  // Your code here
+};
+
+console.log(calculateChange(1787, 2000));
+console.log(calculateChange(2623, 4000));
+console.log(calculateChange(501, 1000));
+
+// Expected output:
+// { twoDollar: 1, dime: 1, penny: 3 }
+// { tenDollar: 1, twoDollar: 1, oneDollar: 1, quarter: 3, penny: 2 }
+// { twoDollar: 2, quarter: 3, dime: 2, penny: 4 }

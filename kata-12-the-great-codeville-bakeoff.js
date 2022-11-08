cde013f5b8af0a09a67b1abdd224b016cc50b652
Mark Bieganek <mark.bieganek@telus.com>
2022-11-07 19:17:43 -0700

    Refactor for re-usability and add comments

--- kata-12-the-great-codeville-bakeoff.js
+++ kata-12-the-great-codeville-bakeoff.js
@@ -1,5 +1,18 @@
+/**
+ * Function that returns the name of a recipe that can be made with 
+ * ingredients on hand. Receives a list of ingredients found at two
+ * bakery locations, as well as a recipe list
+ * @param {Array<string>} bakeryA - A collection of ingredients found 
+ * at this location
+ * @param {Array<string>} bakeryB - A collection of ingredients found 
+ * at this location
+ * @param {Array.<{name: string, ingredients: Array}>} recipes - A 
+ * collection of recipes and the required ingredients
+ * @returns {string} Name of the recipe that can be made
+ */
 const chooseRecipe = function(bakeryA, bakeryB, recipes) {
 
+  // Data set of bakery locations and ingredients in stock
   const locations = [
     {
       name: 'bakeryA',
@@ -10,56 +23,56 @@ const chooseRecipe = function(bakeryA, bakeryB, recipes) {
       ingredientInventory: bakeryB,
     }
   ];
-  
-  const shoppingList = {
-    recipeName: '',
-    bakeryA: [],
-    bakeryB: [],
-    ingredientsNeeded: [],
-  };
 
+  // A collection of shopping lists
+  const shoppingLists = [];
+
+  // Loop through recipes, and build shopping lists for each
   for (let recipe in recipes) {
     let localIndex = -1;
-    shoppingList.recipeName = recipes[recipe].name;
-    shoppingList.bakeryA = [];
-    shoppingList.bakeryB = [];
-    shoppingList.ingredientsNeeded = recipes[recipe].ingredients;
-
+    // Create a shopping list for this recipe
+    const myShoppingList = new ShoppingList(recipes[recipe].name, [], recipes[recipe].ingredients)
+    // Check if we have ingredients already on-hand in our bakery locations
     for (let location of locations) {
-      for (let ingredient in shoppingList.ingredientsNeeded) {
-        localIndex = location.ingredientInventory.indexOf(shoppingList.ingredientsNeeded[ingredient]);
-        if (localIndex > -1) { // Ingredient found
-          shoppingList[location.name].push(shoppingList.ingredientsNeeded[ingredient]);
-          shoppingList.ingredientsNeeded.splice(ingredient, 1);
+      // Find a specific ingredient
+      for (let ingredient in myShoppingList.needIngredients) {
+        localIndex = location.ingredientInventory.indexOf(myShoppingList.needIngredients[ingredient]);
+        // Ingredient was found at this location
+        if (localIndex > -1) {
+          // Add this ingredient and location it was found to our found ingredients
+          myShoppingList.haveIngredients.push(
+            {
+              ingredient: myShoppingList.needIngredients[ingredient],
+              location: location.name,
+            }
+          );
+          // Remove this ingredient from the needed ingredients list
+          myShoppingList.needIngredients.splice(ingredient, 1);
         };
       };
     };
-
-    if (shoppingList.ingredientsNeeded.length === 0) {
-      break;
-    };
-
+    // Add this shopping list to the collection of shopping lists
+    shoppingLists.push(myShoppingList);
   };
 
-  return shoppingList.ingredientsNeeded.length === 0 ? shoppingList.recipeName : '';
+  // Collect the names of all recipes that we can make with ingredients on hand
+  const canMakeRecipes = [];
+  for (let list of shoppingLists) {
+    if (list.needIngredients.length === 0) {
+      canMakeRecipes.push(list.recipeName);
+    }
+  }
+  
+  // Return the name of a recipe we can make with ingredients on hand
+  return canMakeRecipes.length > 0 ? canMakeRecipes[0] : '';
+    
+  // A shopping list for a recipe
+  function ShoppingList (name, haveIngredients, needIngredients) {
+    this.recipeName = name,
+    this.haveIngredients = haveIngredients,
+    this.needIngredients = needIngredients
+  };
 
-
-   /**
-   * Function that finds the index of an item in an array
-   * @param {Array} searchIn - The array to search
-   * @param {*} itemToFind - The item to find
-   * @returns {number} The index position where the substring was found
-   */
-    function findIndexInArray(searchIn, itemToFind) {
-      let index = -1; // returns -1 if item not found
-      for (let i = 0; i < searchIn.length; i++) {
-        if (searchIn[i] === itemToFind) {
-          index = i;
-          break;
-        };
-      };
-      return index;
-    };
 };
 
 let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
@@ -102,10 +115,10 @@ console.log(chooseRecipe(bakeryA, bakeryB, recipes));
 
 /*
 
-We ingredientsNeeded a way to find the index of an item in an array (bakery ingredient arrays)`
+We needIngredients a way to find the index of an item in an array (bakery ingredient arrays)`
   - findIndexInArray()
     - returns -1 if not found
     - returns index # of matched item if found
-We ingredientsNeeded a way to 
+We needIngredients a way to 
 
 */

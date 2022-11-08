3115e10cccf172e4be47dbdc5e17b2219b03958d
Mark Bieganek <mark.bieganek@telus.com>
2022-11-08 16:38:57 -0700

    Retrieve denomination types to iterable array

--- kata-14-change-calculator.js
+++ kata-14-change-calculator.js
@@ -6,36 +6,34 @@
  */
 const calculateChange = function(total, cash) {
   
-  // A way to solve how many times a number can fit in another
-  function getQuotient(dividend, divisor, includeRemainder = true) {
-    let result = dividend / divisor;
-    return includeRemainder ? result : Math.floor(result);
-  }
-  
   // Add currency denominations and their value in cents
   const kvDenominations = {
-    2000: "twentyDollar",
-    1000: "tenDollar",
-    500: "fiveDollar",
-    200: "twoDollar",
-    100: "oneDollar",
-    25: "quarter",
-    10: "dime",
-    5: "nickel",
-    1: "penny",
+    twentyDollar: 2000,
+    tenDollar: 1000,
+    fiveDollar: 500,
+    twoDollar: 200,
+    oneDollar: 100,
+    quarter: 25,
+    dime: 10,
+    nickel: 5,
+    penny: 1,
   }
 
   // Calculate the total change required in cents
   const changeTotal = cash - total;
   
-  // Track the total change remaining
+  // Add variable to track the total change remaining
   let changeRemaining = changeTotal;
+
+  // Retrieve the denomination types
+  Object.keys(kvDenominations)
+
 
 };
 
-console.log(calculateChange(1787, 2000));
-console.log(calculateChange(2623, 4000));
-console.log(calculateChange(501, 1000));
+// console.log(calculateChange(1787, 2000));
+// console.log(calculateChange(2623, 4000));
+console.log(calculateChange(501, 1000)); // 499 cents change
 
 // Expected output:
 // { twoDollar: 1, dime: 1, penny: 3 }

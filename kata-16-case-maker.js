52a0e44182d7c8e10ec1b85139d12c543ad6707c
Mark Bieganek <mark.bieganek@telus.com>
2022-11-09 10:43:35 -0700

    Add makeCase() and move case conversion logic within

--- kata-16-case-maker.js
+++ kata-16-case-maker.js
@@ -1,3 +1,46 @@
+const makeCase = function(input, cases) {
+  let regex;
+  let newString = input;
+  if (Array.isArray(cases) === false) cases = [cases];
+  for (let thisCase of cases) {
+    switch(thisCase) {
+      case "camel":
+        regex = /(\s)[a-z]/g;
+        newString = newString.replace(regex, (newString) => newString.toUpperCase().replace(/ /gi, ''));
+        break;
+      case "pascal":
+        regex = /(^|\s)[a-z]/g;
+        newString = newString.replace(regex, (newString) => newString.toUpperCase().replace(/ /gi, ''));
+        break;
+      case "snake":
+        regex = /[^a-z\s]/gi;
+        newString = newString.replace(regex, '');
+        regex = /\s/gi;
+        newString = newString.replace(regex, '_');
+        break;
+      case "kebab":
+        regex = /[^a-z\s]/gi;
+        newString = newString.replace(regex, '');
+        regex = /\s/gi;
+        newString = newString.replace(regex, '-');
+        break;
+      case "title":
+        regex = /(^|\s)[a-z]/g;
+        newString = newString.replace(regex, (newString) => newString.toUpperCase());
+        break;
+      case "vowel":
+        regex = /[aeiou]/g;
+        newString = newString.replace(regex, (newString) => newString.toUpperCase());
+        break;
+      case "consonant":
+        regex = /[bcdfghjklmnpqrstvwxyz]/g;
+        newString = newString.replace(regex, (newString) => newString.toUpperCase());
+        break;
+    }
+  }
+  return newString;
+}
+
 /**
  * Function that converts a string to camelCase
  * @param {string} input The string to convert to camelCase
@@ -11,7 +54,7 @@
    - is followed by any alphabetical character
    - g for global search (find all)
   */
-   const regex = /(\s)[a-z]/g;
+  const regex = /(\s)[a-z]/g;
   newString = input.replace(regex, (input) => input.toUpperCase().replace(/ /gi, ''));
   return newString;
 };
@@ -104,10 +147,22 @@ const consonantCase = function(input) {
 
 
 
-console.log(camelCase("this is a string", "camel"));
-console.log(pascalCase("this is a string", "pascal"));
-console.log(snakeCase("this is a string", "snake"));
-console.log(kebabCase("this is a string", "kebab"));
-console.log(titleCase("this is a string", "title"));
-console.log(vowelCase("this is a string", "vowel"));
-console.log(consonantCase("this is a string", "consonant"));
+// console.log(camelCase("this is a string", "camel"));
+// console.log(pascalCase("this is a string", "pascal"));
+// console.log(snakeCase("this is a string", "snake"));
+// console.log(kebabCase("this is a string", "kebab"));
+// console.log(titleCase("this is a string", "title"));
+// console.log(vowelCase("this is a string", "vowel"));
+// console.log(consonantCase("this is a string", "consonant"));
+// console.log(makeCase("this is a string", ["upper", "snake"]));
+
+console.log(makeCase("this is a string", "camel"))
+console.log(makeCase("this is a string", "pascal"))
+console.log(makeCase("this is a string", "snake"))
+console.log(makeCase("this is a string", "kebab"))
+console.log(makeCase("this is a string", "title"))
+console.log(makeCase("this is a string", "vowel"))
+console.log(makeCase("this is a string", "consonant"))
+// console.log(makeCase("this is a string", ["upper", "snake"]))
+
+

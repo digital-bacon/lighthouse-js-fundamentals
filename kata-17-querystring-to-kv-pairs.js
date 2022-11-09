eb498c30d734697acf5812c3b37e206ef577dd12
Mark Bieganek <mark.bieganek@telus.com>
2022-11-09 16:54:57 -0700

    Enchance unit test accuracy and output

--- kata-17-querystring-to-kv-pairs.js
+++ kata-17-querystring-to-kv-pairs.js
@@ -1,6 +1,6 @@
 /**
- * Function that accepts a querystring and returns key value pairs
- * @param {string} text - the querystring to decode
+ * Function that accepts a query string and returns key value pairs
+ * @param {string} text - the query string to decode
  * @returns {<{key: string, value: string}>} key value pairs
  */
 const urlDecode = function(text) {
@@ -21,15 +21,75 @@ const urlDecode = function(text) {
   return kvQuerystring;
 };
 
-console.log(urlDecode("duck=rubber"));
-console.log(urlDecode("bootcamp=Lighthouse%20Labs"));
-console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain"));
-console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain").weather);
+// Expected output: {duck: "rubber"}
+result(
+  parseResult(
+    deepEqual(
+      urlDecode("duck=rubber"), {duck: "rubber"}
+      ), true, `{duck: "rubber"}`
+    ), `{duck: "rubber"}`
+  )
 
-/*
-EXPECTED OUTPUT:
-{duck: "rubber"}
-{bootcamp: "Lighthouse Labs"}
-{city: "Vancouver", weather: "lots of rain"}
-"lots of rain"
-*/
+// Expected output: {bootcamp: "Lighthouse Labs"}
+result(
+  parseResult(
+    deepEqual(
+      urlDecode(
+        "bootcamp=Lighthouse%20Labs"
+      ), {bootcamp: "Lighthouse Labs"}
+    ), true, `{bootcamp: "Lighthouse Labs"}`
+  ), `{bootcamp: "Lighthouse Labs"}`
+)
+
+// Expected output: {city: "Vancouver", weather: "lots of rain"}
+result(
+  parseResult(
+    deepEqual(
+      urlDecode(
+        "city=Vancouver&weather=lots%20of%20rain"
+      ), {city: "Vancouver", weather: "lots of rain"}
+    ), true, `{city: "Vancouver", weather: "lots of rain"}`
+  ), `{city: "Vancouver", weather: "lots of rain"}`
+)
+
+// Expected output: "lots of rain"
+result(
+  urlDecode(
+    "city=Vancouver&weather=lots%20of%20rain"
+  ).weather, "lots of rain"
+)
+
+function result(output, expected) {
+  if (output === expected) {
+    console.log(`\x1b[32mTEST PASSED\n\x1b[36mresult:\t\t${output}\n\x1b[0mexpected:\t${expected}\n----------`);
+  } else {
+    console.log(`\x1b[33mTEST FAILED\n\x1b[36mresult\t\t${output}\n\x1b[0mexpected:\t${expected}\n----------`);
+  }
+}
+
+function parseResult(result, expectation, output) {
+  if (result === expectation) return output;
+}
+
+function deepEqual(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (const key of keys1) {
+    const val1 = object1[key];
+    const val2 = object2[key];
+    const areObjects = isObject(val1) && isObject(val2);
+    if (
+      areObjects && !deepEqual(val1, val2) ||
+      !areObjects && val1 !== val2
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
+function isObject(object) {
+  return object != null && typeof object === 'object';
+}

768fcfdba5eb56f678a862c7d19486f52c6c0acd
Mark Bieganek <mark.bieganek@telus.com>
2022-11-08 23:07:58 -0700

    Refactor to standardize approach

--- kata-09-case-maker.js
+++ kata-09-case-maker.js
@@ -3,23 +3,26 @@
  * @param {string} input The string to convert to camelCase
  * @returns {string} The converted string
  */
-const camelCase = function(input) {
+ const camelCase = function(input) {
   let newString = '';
-  let capitalizeNext = false;
-  // Find spaces, remove them, and capitalize next letter
-  for (let i = 0; i < input.length; i++) {
-    // Space was found
-    if (input[i] === ' ') {
-      capitalizeNext = true; // Flag next letter for capitalization
-      continue; // Do not add the space to the newString
-    } else if (capitalizeNext) {
-      // Add this letter to newString, but capitalize it first
-      newString += input[i].toUpperCase();
-      capitalizeNext = false; // Reset flag
-    } else {
-    // Add this letter to newString as-is with no changes
-    newString += input[i];
+  let trimmed = input.trim(); // Remove leading and trailing spaces
+  // Store each word in a collection for easier reference
+  let words = trimmed.split(' ');
+  // Check each word in the collection
+  for (let word in words) {
+    // Don't do anything with the first word
+    if (parseInt(word) > 0) {
+      // Drop spaces
+      if (words[word] === ' ') {
+        words.splice(word, 1);
+        continue;
+      } else {
+        // Make this word uppercase
+        words[word] = words[word][0].toUpperCase() + words[word].slice(1)
+      }
     }
+    // Export the words that we've converted
+    newString += words[word];
   }
   return newString;
 };

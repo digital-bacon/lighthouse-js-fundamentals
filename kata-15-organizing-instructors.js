b3d5eac6b271d3a6e33dfe23f353f96e79c1f2d5
Mark Bieganek <mark.bieganek@telus.com>
2022-11-08 22:06:50 -0700

    Add constructor function and return course name

--- kata-15-organizing-instructors.js
+++ kata-15-organizing-instructors.js
@@ -1,12 +1,36 @@
 /**
  * Function that, when given a list of instructors, creates a single 
  * object to organize them based on their course.
- * @param {{name: string, course: string}} instructors - Instructor 
+ * @param {Array<{name: string, course: string}>} instructors - Instructor 
  * names and the course they teach
  * @returns {{course: Array<string>}} Courses with names of instructors
  */
 const organizeInstructors = function(instructors) {
-  // Put your solution here
+  
+  const sortedInstructors = new SortInstructors(instructors);
+  return sortedInstructors.sortedByCourse;
+  
+  /**
+   * Constructor function that receives a transaction amount and 
+   * payment received, and parses suggested change
+   * @param {Array<{name: string, course: string}>} instructors - Instructor 
+   * names and the course they teach
+   */
+   function SortInstructors(instructors) {
+    this.sortByCourse = function() {
+      // Add a way to remember the sorted records
+      const sorted = {};
+      // Look at each instructor record
+      for (let instructor of instructors) {
+       // Does this course already exist in the sorted collection?
+        if (sorted.hasOwnProperty(instructor.course) === false) {
+          sorted[`${instructor.course}`] = []; // Add the course
+        };
+      }
+      return sorted;
+    },
+    this.sortedByCourse = this.sortByCourse()
+  };
 };
 
 console.log(organizeInstructors([

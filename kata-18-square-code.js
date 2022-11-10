ee701f0423ce0a7ea6e50264c72747bb5bd75e0a
Mark Bieganek <mark.bieganek@telus.com>
2022-11-09 18:08:43 -0700

    Fix unit tests, remove spaces from message

--- kata-18-square-code.js
+++ kata-18-square-code.js
@@ -4,20 +4,26 @@
  * @returns {string} The encoded message
  */
 const squareCode = function(message) {
-  
+  /* 
+  In Square Code, the spaces are removed from the english text and 
+  the characters are written into a square (or rectangle).
+  */
+  // Remove spaces
+  message = message.replace(/\s{1}/g, '');
+  return message;
 };
 
 // Expected output: clu hlt io  
-console.log(result(squareCode("chill out"),`clu hlt io `);
+result(squareCode("chill out"),`clu hlt io `);
 
 // Expected output: fto ehg ee dd
-console.log(result(squareCode("feed the dog"),`fto ehg ee dd`);
+result(squareCode("feed the dog"),`fto ehg ee dd`);
 
 // Expected output: hae and via ecy
-console.log(result(squareCode("have a nice day"),`hae and via ecy`);
+result(squareCode("have a nice day"),`hae and via ecy`);
 
 // Expected output: imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau
-console.log(result(squareCode("if man was meant to stay on the ground god would have given us roots"),`imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau`);
+result(squareCode("if man was meant to stay on the ground god would have given us roots"),`imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau`);
 
 function result(output, expected) {
   if (output === expected) {

60203643abb5f36237d1b54da40b1c405a38efb7
Mark Bieganek <mark.bieganek@telus.com>
2022-11-09 20:58:28 -0700

    Populate rows by filling them equally from top to bottom

--- kata-18-square-code.js
+++ kata-18-square-code.js
@@ -1,6 +1,6 @@
 /**
  * Function that encodes a given message using square code
- * @param {*} message The message to encode
+ * @param {string} message The message to encode
  * @returns {string} The encoded message
  */
 const squareCode = function(message) {
@@ -13,7 +13,7 @@ const squareCode = function(message) {
   // Remove spaces
   message = message.replace(/\s{1}/g, '');
   // Determine how many rows and characters per row to form a square
-  const charactersPerRow = Math.round(Math.sqrt(message.length));
+  const charactersPerRow = Math.ceil(Math.sqrt(message.length));
   // Add a way to store the square encoded message in rows
   let messageAcrossRows = [];
   // Add an appropriate number of empty rows for our encoded message
@@ -26,28 +26,35 @@ const squareCode = function(message) {
       messageAcrossRows[y].push(message[i + y]);
     }
   }
+  // Extract the encoded message into a string
+  for (const row in messageAcrossRows) {
+    for (const character in messageAcrossRows[row]) {
+      if (typeof messageAcrossRows[row][character] === "undefined") {
+        continue;
+      }
+      encodedMessage += messageAcrossRows[row][character];
+    }
+    encodedMessage += ' ';
+  }
+  // Trim trailing whitespace
+  encodedMessage = encodedMessage.trim();
 
-  return messageAcrossRows;
+  return encodedMessage;
 };
 
-console.log(squareCode("chill out"))
-
-// // Expected output: clu hlt io  
-// result(squareCode("chill out"),`clu hlt io`);
-
-// // Expected output: fto ehg ee dd
-// result(squareCode("feed the dog"),`fto ehg ee dd`);
-
-// // Expected output: hae and via ecy
-// result(squareCode("have a nice day"),`hae and via ecy`);
-
-// // Expected output: imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau
-// result(squareCode("if man was meant to stay on the ground god would have given us roots"),`imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau`);
+// Expected output: clu hlt io  
+result(squareCode("chill out"),`clu hlt io`);
+// Expected output: fto ehg ee dd
+result(squareCode("feed the dog"),`fto ehg ee dd`);
+// Expected output: hae and via ecy
+result(squareCode("have a nice day"),`hae and via ecy`);
+// Expected output: imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau
+result(squareCode("if man was meant to stay on the ground god would have given us roots"),`imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau`);
 
 function result(output, expected) {
   if (output === expected) {
-    console.log(`\x1b[32mTEST PASSED\n\x1b[36mresult:\t\t${output}\n\x1b[0mexpected:\t${expected}\n----------`);
+    console.log(`\n\x1b[32mTEST PASSED\n\x1b[36mresult:\n\x1b[0m`, output, `\n\x1b[36mexpected:\x1b[0m\n`, expected, `\n\n----------`);
   } else {
-    console.log(`\x1b[33mTEST FAILED\n\x1b[36mresult\t\t${output}\n\x1b[0mexpected:\t${expected}\n----------`);
+    console.log(`\n\x1b[33mTEST FAILED\n\x1b[36mresult:\n\x1b[0m`, output, `\n\x1b[36mexpected:\x1b[0m\n`, expected, `\n\n----------`);
   }
 }

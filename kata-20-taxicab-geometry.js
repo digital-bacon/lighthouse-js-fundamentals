c6fbed0840945683ab440e8aea7d050df2346eb6
Mark Bieganek <mark.bieganek@telus.com>
2022-11-10 16:59:44 -0700

    Add ways to remember cab details and result

--- kata-20-taxicab-geometry.js
+++ kata-20-taxicab-geometry.js
@@ -9,10 +9,23 @@ Constraints:
 1. The driver always starts in the same position, SW corner of grid
 2. The output will always be East and North from start.
 
+Array [5, 6]
+
+
 */
 
 const blocksAway = function(directions) {
-  // Put your solution here
+  // To remember taxi cab position
+  let cabPosition = [0, 6]
+  // To remember taxi cab direction of movement
+  let cabDirectionOfMovement = 'North'
+  // To remember taxi cab distance to travel in the current direction
+  let cabDistanceToTravel = 0;
+  // To remember taxi cab travel distance from start point
+  let blocksAway = {};
+
+  // Report blocks away
+  return blocksAway;
 };
 
 console.log(blocksAway(["right", 2, "left", 3, "left", 1]));

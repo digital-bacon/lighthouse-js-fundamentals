c42ba5bea2abf54796927474b567ee4c92d944f1
Mark Bieganek <mark.bieganek@telus.com>
2022-11-09 20:04:49 -0700

    Refactor to add empty rows first

--- kata-18-square-code.js
+++ kata-18-square-code.js
@@ -14,28 +14,29 @@ const squareCode = function(message) {
   message = message.replace(/\s{1}/g, '');
   // Determine how many rows and characters per row to form a square
   const charactersPerRow = Math.round(Math.sqrt(message.length));
-  // Add a way to store the message in rows
-  let messageInRows = [];
-  // Split the message into rows
-  for (let i = 0; i < message.length; i+=3) {
-    messageInRows.push(message.slice(i, (i + 3)));
+  // Add a way to store the square encoded message in rows
+  let messageAcrossRows = [];
+  // Add an appropriate number of empty rows for our encoded message
+  for (let i = 1; i <= charactersPerRow; i++) {
+    messageAcrossRows.push([]);
   }
 
-  return messageInRows;
+  return messageAcrossRows;
 };
 
+console.log(squareCode("chill out"))
 
-// Expected output: clu hlt io  
-result(squareCode("chill out"),`clu hlt io`);
+// // Expected output: clu hlt io  
+// result(squareCode("chill out"),`clu hlt io`);
 
-// Expected output: fto ehg ee dd
-result(squareCode("feed the dog"),`fto ehg ee dd`);
+// // Expected output: fto ehg ee dd
+// result(squareCode("feed the dog"),`fto ehg ee dd`);
 
-// Expected output: hae and via ecy
-result(squareCode("have a nice day"),`hae and via ecy`);
+// // Expected output: hae and via ecy
+// result(squareCode("have a nice day"),`hae and via ecy`);
 
-// Expected output: imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau
-result(squareCode("if man was meant to stay on the ground god would have given us roots"),`imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau`);
+// // Expected output: imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau
+// result(squareCode("if man was meant to stay on the ground god would have given us roots"),`imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau`);
 
 function result(output, expected) {
   if (output === expected) {

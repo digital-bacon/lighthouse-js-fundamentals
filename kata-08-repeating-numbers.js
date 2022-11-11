577f18363c1adc126c15e7601a1ce1f0708fb870
Mark Bieganek <mark.bieganek@telus.com>
2022-11-11 11:32:01 -0700

    Add semicolons for consistency

--- kata-08-repeating-numbers.js
+++ kata-08-repeating-numbers.js
@@ -5,7 +5,7 @@
  * second will be the amount of times to repeat that item
  * @returns {*} With the result of the repetition
  */
-const repeatNumbers = function(data) {
+ const repeatNumbers = function(data) {
   let output = '';
   // Loop data array
   for (let i = 0; i < data.length; i++) {
@@ -14,13 +14,13 @@ const repeatNumbers = function(data) {
     // Repeat the string
     for (let y = 0; y < repeatAmount; y++) {
         output += repeatString;
-    }
+    };
     // Add a comma if more strings will follow
     if ((i + 1) < data.length) output += ', ';
-  }
+  };
   return output;
 };
 
-console.log(repeatNumbers([[1, 10]]));
-console.log(repeatNumbers([[1, 2], [2, 3]]));
-console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
+console.log(repeatNumbers([[1, 10]]), '=?', '1111111111');
+console.log(repeatNumbers([[1, 2], [2, 3]]), '=?', '11, 222');
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]), '=?', '10101010, 343434343434, 9292');

c087a031fbb860d4bdc6fb8145e45e798e4a9392
Mark Bieganek <mark.bieganek@telus.com>
2022-11-11 15:40:47 -0700

    Expand example

--- abstraction.js
+++ abstraction.js
@@ -18,7 +18,7 @@ const coneVolume = (radius, height) => {
 
 const prismVolume = (height, width, depth) => {
   // Formula: V=(wd)h, or in JS: ((width * depth) * height)
-  return (calculate(area, height, width) * height);
+  return (calculate(area, width, depth) * height);
 };
 
 const calculate = (logic, ...args) => {
@@ -28,3 +28,8 @@ const calculate = (logic, ...args) => {
 console.log(calculate(sphereVolume, 10));
 console.log(calculate(coneVolume, 3, 5));
 console.log(calculate(prismVolume, 3, 4, 5));
+
+// And an example with mapping, to show the power of this approach
+const prisms = [[3, 4, 5], [4, 2, 10], [5, 2, 7]] // 3 prisms
+// Output an array with the volume of each prism
+console.log(prisms.map(element => calculate(prismVolume,element[0], element[1], element[2])))
